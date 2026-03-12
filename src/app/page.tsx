5fcbc8b946ee034289c442c5002900ca89cdcc39
sh.irani <ASUS@DESKTOP-MHPSGQJ>
2026-03-12 07:46:20 +0330

    todolist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import FilterTabs from "@/features/todo/components/FilterTabs";
 import TodoInput from "@/features/todo/components/TodoInput";
-import TodoList from "@/features/todo/components/TodoList";
+import TodoList from "@/features/todo/components/todoList/TodoList";
 import { Typography } from "@mui/material";
 
 export default function Dashboard() {
   return (
     <>
-    <FilterTabs />
+      <FilterTabs />
       <Typography variant="h4" sx={{ mb: 3 }}>
         Dashboard
       </Typography>
---
+++ src/features/todo/components/todoList/TodoList.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import TodoItem from "../TodoItem";
+import { useTodoList } from "./useTodoList";
+
+export default function TodoList() {
+  const { handleDragEnd, todos } = useTodoList();
+  return (
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext
+        items={todos.map((t) => t.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </SortableContext>
+    </DndContext>
+  );
+}
